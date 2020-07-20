a544b02846cf479192911e972dcdf3ddb940b14e
Anurag Raina <rainakalhan90@gmail.com>
2020-07-20 11:34:29 +0530

    Setup files to be deployed on heroku

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
 	try {
-		await mongoose.connect('mongodb://localhost:27017/Hexaland', {
+		await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Hexaland', {
 			useNewUrlParser    : true,
 			useUnifiedTopology : true
 		});
--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.use(express.json({ extended: false }));
 
 app.use('/api', hexalandRoute);
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
 app.listen(PORT, () => {
 	console.log(`Server listening on PORT ${PORT}`);
 });
